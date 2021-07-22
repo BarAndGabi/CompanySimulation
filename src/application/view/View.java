8f3d6d933953272b3585d88ad848daf35aa38393
GabrieLempert <gabriel.lempert@s.afeka.ac.il>
2021-07-22 15:28:19 +0300

    Update View.java
    Almost final changes to department interface

--- src/application/view/View.java
+++ src/application/view/View.java
@@ -5,9 +5,11 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
@@ -25,6 +27,7 @@ public class View implements AbstractView {
 			new Button("Show Screen"), new Button("Change Prefrence"), new Button("Show Simulations Results"),
 			new Button("Save"), new Button("Exit") };
 	private VBox vBoxForButtons;
+	private ComboBox<Integer> c1 = new ComboBox<Integer>();
 	private Image logo = new Image(getClass().getResourceAsStream("logo.png"));
 
 	public View(Stage theStage) {
@@ -108,23 +111,52 @@ public class View implements AbstractView {
 		sp.getChildren().add(choosePreference);
 		BorderPane bp = new BorderPane();
 		VBox choosWorkMethod = new VBox();
+		ToggleGroup tg = new ToggleGroup();
 		getRd()[4].setText("Early");
 		getRd()[5].setText("Late");
 		getRd()[6].setText("Home");
 		getRd()[7].setText("Regular");
 		for (int i = 4; i < rd.length; i++) {
 			choosWorkMethod.getChildren().add(rd[i]);
+			tg.getToggles().add(rd[i]);
+
 		}
 		Label l3 = new Label("Choose work method: ");
 		l3.setPadding(new Insets(15));
 		bp.setLeft(l3);
 		bp.setCenter(choosWorkMethod);
+		HBox hb = new HBox();
+		getRd()[4].setOnAction(e -> {
+			hb.getChildren().clear();
+			Label l4 = new Label("how early would you like to be");
+			getC1().getItems().clear();
+			for (int i = 1; i < 9; i++) {
+				getC1().getItems().add(i);
+			}
+			getC1().setValue(1);
+			hb.getChildren().addAll(l4, getC1());
+			hb.setSpacing(15);
+			bp.setRight(hb);
+
+		});
+
+		getRd()[5].setOnAction(e -> {
+			hb.getChildren().clear();
+			Label l4 = new Label("how late would you like to be");
+			getC1().getItems().clear();
+			for (int i = 1; i < 8; i++) {
+				getC1().getItems().add(i);
+			}
+			getC1().setValue(1);
+			hb.getChildren().addAll(l4, getC1());
+			hb.setSpacing(15);
+			bp.setRight(hb);
+		});
+
 		sp.getChildren().add(bp);
 		changePane.setLeft(sp);
 
 	}
-
-
 
 	public TextField getTf() {
 		return tf;
@@ -137,5 +169,14 @@ public class View implements AbstractView {
 	@Override
 	public void registerListener(CompanySimulationController companySimulationController) {
 
+	}
+
+	public ComboBox<Integer> getC1() {
+		return c1;
+	}
+
+	public void setC1() {
+		ComboBox<Integer> c1 = new ComboBox<Integer>();
+		this.c1 = c1;
 	}
 }
