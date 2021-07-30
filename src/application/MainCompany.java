cad24344050c0b422d64c6803bac43be7d873cdd
barmoshe <1barmoshe1@gmail.com>
2021-07-30 15:05:11 +0300

    Create MainCompany.java

---
+++ src/application/MainCompany.java
@@ -0,0 +1,24 @@
+package application;
+
+import application.controller.CompanySimulationController;
+
+import application.model.Company;
+import application.model.CompanyInterface;
+
+import application.view.View;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainCompany extends Application {
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		View View = new View(primaryStage);
+		CompanyInterface Model = new Company();
+		CompanySimulationController controller = new CompanySimulationController(Model, View);
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+}
